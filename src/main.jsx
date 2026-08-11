3c6c300b71796e164d02d9f1deafdacaf927cec6
Gopal1833 <krishnagopal102006@gmail.com>
2026-08-11 13:09:13 +0530

    add github actions

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './styles/design-tokens.css'
 import './styles/global.css'
@@ -8,10 +8,10 @@ import { ToastProvider } from './components/common/Toast/Toast'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ToastProvider>
         <App />
       </ToastProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
